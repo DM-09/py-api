fadeea48bbe084f7a5f244ec356e76bc4e255435
DM <happydm09@naver.com>
2023-07-05 16:18:27 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, Response
 import requests as req
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/<string:URL>", methods=['GET'])
 def GetInfo(URL):
