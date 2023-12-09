e5ab416cff3fb262cc4d10a7d29a03394cb6ee7d
dongmin <happydm09@naver.com>
2023-12-09 18:39:42 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, jsonify, Response
 import requests as req
 from flask_cors import CORS
@@ -5,9 +6,19 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/<string:URL>", methods=['GET'])
-def GetInfo(URL):
+@app.route("/", methods=['GET'])
+def OldVersion(URL):
+    print(URL)
     URL = URL.replace('~', '/').replace('$', '?')
+
+    try: data = req.get(URL).json()
+    except: return Response('Error', 404)
+    return jsonify(data), 200
+
+@app.route("/q", methods=['GET'])
+def NewVersion():
+    URL = flask.request.url
+    URL = URL[URL.index('q?')+2:]
 
     try: data = req.get(URL).json()
     except: return Response('Error', 404)
